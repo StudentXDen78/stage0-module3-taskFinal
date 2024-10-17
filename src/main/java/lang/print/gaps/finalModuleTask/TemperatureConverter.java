301283dff11725113d75c5b30a7acc50b6ea52d5
xden78 <venera.opt@gmail.com>
2024-10-17 23:24:01 +0300

    attempt 1

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        // ( °C × 9/5) + 32 =  °F
+        float temperatureFahrenheit = (temperatureCelsius * (float)(9 / 5)) + 32;
+        System.out.println(temperatureFahrenheit);
     }
 }
